e8565c5c672b9a14097b098751a8b60113c6da75
Jordan D'Souza <16809201+Jouzaj@users.noreply.github.com>
2023-08-30 10:00:33 +1200

    Fixed mV scaler

--- test/drive_calculations.py
+++ test/drive_calculations.py
@@ -1,8 +1,14 @@
-MAX_THERSHOLD: int = 100
+import numpy
 
+MAX_THERSHOLD: int = 127
+maximum: int = 12000
+MAX_SCALED_JOYSTICK: int = 127
+
+
+# 1 value increases mV by 94.4881889763783 FLI!
 while True:
     joystick_input: str = input("Joystick\t")
-    print(joystick_input.isalnum())
+    
 
     if joystick_input.isdigit() or joystick_input.startswith('-') and joystick_input[1:].isdigit():
         
@@ -14,5 +20,14 @@ while True:
 
         cubic_scaling: float = pow(joystick_value / 100, 3) * MAX_THERSHOLD
         print(f"Cubic scalling {cubic_scaling}")
+
+        squared_scaling: float = pow(joystick_value, 2) / maximum * numpy.sign(joystick_value)
+        print(f"squared scalling {squared_scaling}")
+
+        
+        numerator = joystick_value-(-127) 
+        denomorator = 127 - (-127)
+        scaled_milivolts = -12000 + (numerator/ denomorator) * 24000
+        print(f"milivolts  {scaled_milivolts}")
     continue
 
