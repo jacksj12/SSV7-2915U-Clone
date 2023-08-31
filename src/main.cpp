69a27fed61192e818626bf46cc33f00b9980b83e
Jordan D'Souza <16809201+Jouzaj@users.noreply.github.com>
2023-08-31 19:36:24 +1200

    Remove other functions

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,7 @@ void disabled() {}
  * This task will exit when the robot is enabled and autonomous or opcontrol
  * starts.
  */
-void competition_initialize() {}
+// void competition_initialize() {}
 
 /**
  * Runs the user autonomous code. This function will be started in its own task
@@ -58,7 +58,7 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+// void autonomous() {}
 
 /**
  * Runs the operator control code. This function will be started in its own task
