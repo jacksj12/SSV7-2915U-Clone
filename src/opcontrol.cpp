2a788185336d661e0163cba68ff1d86d1c6dc93a
Jordan D'Souza <16809201+Jouzaj@users.noreply.github.com>
2023-09-14 18:47:06 +1200

    formatting everything ✨

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -3,10 +3,6 @@
 
 // https://pros.cs.purdue.edu/v5/api/cpp/index.html
 
-
-
-
-
 /**
  * Runs the operator control code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
@@ -25,41 +21,37 @@ void opcontrol(void)
 {
     while (true)
     {
-        int8_t joystick_scaling_ammount = 2;    // scales the joystick inputs for the algorithim to not exceed voltage threshold.
-                                                // Allows for greater movement when only one joystick signal is active.
+        int8_t joystick_scaling_ammount = 2; // scales the joystick inputs for the algorithim to not exceed voltage threshold.
+                                             // Allows for greater movement when only one joystick signal is active.
 
         // Get Joysticks.
-        int8_t main_left_joystick =  controller_main.get_analog(ANALOG_LEFT_Y); // AXIS 3 of controller_main.
+        int8_t main_left_joystick = controller_main.get_analog(ANALOG_LEFT_Y);        // AXIS 3 of controller_main.
         int8_t main_right_joystick = controller_main.get_analog(ANALOG_RIGHT_X) * -1; // AXIS 1 of controller_main. Invert joystick
 
-
-
+        // Determines the ammount of active joystick signals.
         if (main_left_joystick == 0 || main_right_joystick == 0)
-        {
-            joystick_scaling_ammount = 1;
-        }
+            joystick_scaling_ammount = 1; // Sets joystick scalling constant.
 
         // Apply deadzones to Joysticks.
         main_left_joystick = apply_deadzone(main_left_joystick, VERTICAL_DEADZONE);
         main_right_joystick = apply_deadzone(main_right_joystick, HORIZONTAL_DEADZONE);
 
         // Update the drive motors. The move method uses voltage control.
-        drive_left.move((main_left_joystick + main_right_joystick)  / joystick_scaling_ammount);
+        drive_left.move((main_left_joystick + main_right_joystick) / joystick_scaling_ammount);
         drive_right.move((main_left_joystick - main_right_joystick) / joystick_scaling_ammount);
 
-
-        if(controller_main.get_digital_new_press(DIGITAL_L1))
+        // Wings Toggle.
+        if (controller_main.get_digital_new_press(DIGITAL_L1))
         {
-            wing_left_toggle = !wing_left_toggle; 
+            wing_left_toggle = !wing_left_toggle;
             wing_left.set_value(wing_left_toggle);
         }
-        if(controller_main.get_digital_new_press(DIGITAL_R1))
+        if (controller_main.get_digital_new_press(DIGITAL_R1))
         {
             wing_right_toggle = !wing_right_toggle;
             wing_right.set_value(wing_right_toggle);
         }
 
         pros::delay(20); // 20 ms delay for user based interactions
-
     }
 }
