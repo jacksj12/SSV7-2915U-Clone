289bb98d69d4ef7829ac83d7cbfe8e3b105d70cb
Jouzaj <16809201+Jouzaj@users.noreply.github.com>
2023-09-15 19:36:56 +1200

    Removed the LCD init

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "main.h"
+ #include "main.h"
 
 /**
  * A callback function for LLEMU's center button.
@@ -23,10 +23,12 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+	// pros::lcd::initialize();
+	// pros::lcd::set_text(1, "Hello PROS User!");
 
-	pros::lcd::register_btn1_cb(on_center_button);
+	// pros::lcd::register_btn1_cb(on_center_button);
+
+	
 }
 
 /**
