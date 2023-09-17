60a49638d5478af6a8f05e5a4d10b4f4f9326f62
Jordan D'Souza <16809201+Jouzaj@users.noreply.github.com>
2023-09-17 18:38:19 +1200

    added function comment

--- src/disabled.cpp
+++ src/disabled.cpp
@@ -1,3 +1,8 @@
 #include "main.h"
 
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
 void disabled(void){}
